8f27815c113cacb686677225ebec8ad9742a7422
Jigar Patel <8.jigar.patel@gmail.com>
2023-08-16 13:00:54 -0600

    Pt 5 - dark mode using TailwindCSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,38 @@
+import React from "react";
+
 function App() {
+
+  React.useEffect(() => {
+    if (localStorage.getItem('theme') === 'dark') {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+  }, []);
+  
+ const toggleTheme = () => {
+    if (localStorage.getItem('theme') === 'dark') {
+      localStorage.setItem('theme', 'light');
+      document.documentElement.classList.remove('dark');
+    } else {
+      localStorage.setItem('theme', 'dark');
+      document.documentElement.classList.add('dark');
+    }
+  };
+
   return (
-    <>
-      <h1 className="text-center text-6xl py-5">
-        Hello world!
-      </h1>
-    </>
+    <div className="dark:bg-gray-900 dark:text-white h-screen">
+      <div className="container mx-auto p-3">
+        <div className="flex">
+          <div className="flex-grow text-4xl">
+            Hello world!
+          </div>
+          <button type="button" onClick={toggleTheme} className="bg-blue-500 hover:bg-blue-700 text-white p-2 rounded">
+            Toggle Theme
+          </button>
+        </div>
+      </div>
+    </div>
   )
 }
 
